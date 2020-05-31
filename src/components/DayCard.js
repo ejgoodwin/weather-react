406690dfacfada5cd46c0c7b22bcd6d402355399
ejgoodwin <ericagoodwin90@gmail.com>
2020-05-31 19:20:04 +1200

    added imagery

--- src/components/DayCard.js
+++ src/components/DayCard.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 const DayCard = (props) => {
-	const weatherIcon = `owf owf-${props.reading.weather[0].id} owf-2x`;
+	// const weatherIcon = `owf owf-${props.reading.weather[0].id} owf-2x`;
+	const weatherIcon = props.reading.weather[0].id;
 	return (
 		<div className="card">
 			
 			<p>{Math.round(props.reading.main.temp)}&deg;C</p>
-			<i className={weatherIcon}></i>
+			{/*<i className={weatherIcon}></i>*/}
+			<div className={`icon-${weatherIcon} card__icon`}></div>
 		</div>
 	)
 }
--- src/components/WeatherToday.js
+++ src/components/WeatherToday.js
@@ -1,14 +1,20 @@
 import React from 'react';
 
 const WeatherToday = (props) => {
-	const weatherIcon = `owf owf-${props.reading.weather[0].id} owf-5x`;
+	const weatherIcon = props.reading.weather[0].id;
 	return (
 		<div className="weather-today">
-			<span className="weather-today__today">Today</span>
-			<p className="weather-today__text weather-today__text--capitalize">{props.city}, {props.country}</p>
-			<p className="weather-today__text">{Math.round(props.reading.main.temp)}&deg;C</p>
-			<p className="weather-today__text">{props.reading.weather[0].description}</p>
-			<i className={weatherIcon}></i>
+			<div className="weather-today__text-container">
+				<span className="weather-today__today">Today</span>
+				<p className="weather-today__text weather-today__text--capitalize">{props.city}, {props.country}</p>
+				<p className="weather-today__text">{Math.round(props.reading.main.temp)}&deg;C</p>
+				<p className="weather-today__text">{props.reading.weather[0].description}</p>
+			</div>
+			{/*<i className={weatherIcon}></i>*/}
+
+			<div className="weather-today__icon">
+				<div className={`icon-${weatherIcon} custom-icon`}></div>
+			</div>
 		</div>
 	)
 }
